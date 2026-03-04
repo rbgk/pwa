6d34a12025ccc24e30c334d445c9bbde838c6beb
rbgk <50600773+rbgk@users.noreply.github.com>
2026-03-04 14:05:28 -0500

    Implement wake lock request on user interaction

--- script.js
+++ script.js
@@ -6,7 +6,30 @@ if ('wakeLock' in navigator) {
   document.getElementById("wakeLock").innerText = "wakeLock unsupported";
 }
 
-// wakeLock requires User interaction. cannot call it upon initialize
-document.addEventListener('pointerdown', (event) => {
-  document.getElementById("status").innerText = "User tapped or clicked";
+
+// Request wake lock
+async function requestWakeLock() {
+  if ('wakeLock' in navigator) {
+    try {
+      // We don’t track release, just request it
+      await navigator.wakeLock.request('screen');
+      document.getElementById("status").innerText = ('Wake lock requested');
+    } catch (err) {
+      document.getElementById("status").innerText = (`Wake lock request failed: ${err.name}, ${err.message}`);
+    }
+  } else {
+    document.getElementById("status").innerText = ('Wake Lock API not supported in this browser');
+  }
+}
+
+// Detect first user interaction
+document.addEventListener('pointerdown', async () => {
+  await requestWakeLock();
+}, { once: true });
+
+// Re-request wake lock when the page becomes visible again
+document.addEventListener('visibilitychange', async () => {
+  if (document.visibilityState === 'visible') {
+    await requestWakeLock();
+  }
 });
