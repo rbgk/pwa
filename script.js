b12ab6d72840b5cdffc9c6a72a59ce1fdcbfba5b
rbgk <50600773+rbgk@users.noreply.github.com>
2026-03-04 13:41:37 -0500

    Implement user interaction event listener

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ if ('wakeLock' in navigator) {
   // unsupported
   document.getElementById("wakeLock").innerText = "wakeLock unsupported";
 }
+
+// wakeLock requires User interaction. cannot call it upon initialize
+document.addEventListener('pointerdown', (event) => {
+  document.getElementById("status").innerText = "User tapped or clicked";
+});
